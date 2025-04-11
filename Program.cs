49e2acdc50094a5d80684edcaa1f15ad36945b61
MohammedOsamma <mo856915i@gmail.com>
2025-04-12 01:49:22 +0200

    Created 'Algorithms' and 'Sorting' folders &  Added first algorithm: Bubble Sort implementation

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms.Sorting;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 4, 3, 9, 2 };
+
+
+            BubbleSort.Sort(numbers);
+            Console.WriteLine("Sorted Array :" + string.Join(",",numbers));
+        }
+    }
+}
